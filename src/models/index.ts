efbf4287acfe5965fcd6dda7ced262615bcb4ae2
natividadesusana <susanajdsn@gmail.com>
2023-10-11 03:53:39 -0300

    feat: implementing model index

---
+++ src/models/index.ts
@@ -0,0 +1,7 @@
+export * from "./exoplanet-model";
+export * from "./recharge-model";
+export * from "./rechargeInput-model";
+export * from "./station-model";
+export * from "./stationInput-model";
+export * from "./suitablePlanet-model";
+
