70dcd09c75c67c64a86d577fb682020351873bb7
natividadesusana <susanajdsn@gmail.com>
2023-10-11 03:37:25 -0300

    feat: implementing resolver for searching exoplanet data

---
+++ src/graphql/resolvers/planet-resolver.ts
@@ -0,0 +1,22 @@
+import { fetchExoplanetsData } from "../../api/axiosApi";
+import { createNewPlanet } from "../../services/planet-service";
+import { SuitablePlanet } from "../../models/suitablePlanet-model";
+
+const planetResolvers = {
+  Query: {
+    suitablePlanets: async (): Promise<SuitablePlanet[]> => {
+      try {
+        const exoplanets = await fetchExoplanetsData();
+
+        const suitablePlanetsPromises = exoplanets.map((planet) => createNewPlanet(planet));
+        const suitablePlanets = await Promise.all(suitablePlanetsPromises);
+
+        return suitablePlanets.filter((planet) => planet !== null) as SuitablePlanet[];
+      } catch (error) {
+        throw new Error(`Error getting planets: ${error}`);
+      }
+    },
+  },
+};
+
+export default planetResolvers;
